4858c22f6ac371e3cbe1f58d30b4f9ee399a5a31
tony <tony.li@vechain.com>
2019-09-03 15:48:51 +0800

    minor: debug info

--- src/exec-script.ts
+++ src/exec-script.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 import { Framework } from '@vechain/connex-framework'
 import { Driver, SimpleWallet, SimpleNet } from '@vechain/connex.driver-nodejs'
+const debug = require('debug')('sharp:exec')
 
 interface Global extends NodeJS.Global {
     connex: Connex
@@ -15,6 +16,7 @@ export const execScript = async (file: string, endpoint: string) => {
     try {
         fs.accessSync(filePath, fs.constants.F_OK | fs.constants.R_OK)
 
+        debug('prepare connex env')
         const wallet = new SimpleWallet()
         const driver = await Driver.connect(new SimpleNet(endpoint), wallet)
         const connex = new Framework(Framework.guardDriver(driver))
@@ -24,12 +26,16 @@ export const execScript = async (file: string, endpoint: string) => {
         throw new Error('Prepare failed: ' + e.message)
     }
 
+    debug('execute script')
     const con = await import(filePath)
     if (typeof con === 'function') {
+        debug('module.exports = function')
         await con()
     } else if (typeof con.default === 'function') {
+        debug('export default function')
         await con.default()
     } else {
         throw new Error('Cannot locate the task in the script')
     }
+    debug('execute finished')
 }
