5e24598d1d031a6eebc65b835aae74e9a9ed582a
tony <tony.li@vechain.com>
2019-09-06 17:26:35 +0800

    print net info before execute script

--- src/exec-script.ts
+++ src/exec-script.ts
@@ -4,6 +4,12 @@ import { Framework } from '@vechain/connex-framework'
 import { Driver, SimpleWallet, SimpleNet } from '@vechain/connex.driver-nodejs'
 const debug = require('debug')('sharp:exec')
 
+const networks: { [index: string]: string } = {
+    '0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a': 'Mainnet',
+    '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127': 'Testnet',
+    '0x00000000973ceb7f343a58b08f0693d6701a5fd354ff73d7058af3fba222aea4': 'Solo'
+}
+
 export const execScript = async (file: string, endpoint: string, requires: string[]) => {
     const filePath = path.join(process.cwd(), file)
 
@@ -32,12 +38,23 @@ export const execScript = async (file: string, endpoint: string, requires: strin
     }
 
     debug('execute script')
+
+    const printNetInfo = () => {
+        const geneID = global.connex.thor.genesis.id
+        const netName = networks[geneID] ? networks[geneID] : 'UnKnown'
+
+        process.stderr.write(`Connected to ${netName}(#0....${geneID.substr(-8)}) @ ${endpoint}`)
+        process.stderr.write('\n')
+    }
+
     const con = await import(filePath)
     if (typeof con === 'function') {
         debug('module.exports = function')
+        printNetInfo()
         await con()
     } else if (typeof con.default === 'function') {
         debug('export default function')
+        printNetInfo()
         await con.default()
     } else {
         throw new Error('Cannot locate the task in the script')
